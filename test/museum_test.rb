d5d934a5fb3548ac426a4df5f3f6390faf5fe838
Olivia Charles <ocharles92@gmail.com>
2021-01-07 11:33:58 -0500

    Created test for it starts with no exhibits and added exhibits to test setup.

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -2,15 +2,22 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/exhibit'
 require './lib/patron'
-reuire  './lib/museum'
+require './lib/museum'
 
 
 class PatronTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   def test_it_exitsts_and_has_attributes
     assert_instance_of Museum, @dmns
     assert_equal "Denver Museum of Nature and Science", @dmns.name
   end
+
+  def test_it_starts_with_no_exhibits
+    assert_equal [], @dmns.exhibits
+end
