e1cca7ee2c27cc6f9dcd594a637669a430c99b4e
Olivia Charles <ocharles92@gmail.com>
2021-01-07 11:10:02 -0500

    Created test for exists and has atrributes

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -7,4 +7,10 @@ class ExhibitTest < Minitest::Test
   def setup
     @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
   end
+
+  def test_it_exitsts_and_has_attributes
+    assert_instance_of Exhibit, @exhibit
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
+  end
 end
