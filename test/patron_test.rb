ec56a1519ef33e3b99ca7fae858a0a1ad290878a
Olivia Charles <ocharles92@gmail.com>
2021-01-07 11:23:31 -0500

    Created tests for starts with no interests and it can add interests.

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -13,4 +13,15 @@ class PatronTest < Minitest::Test
     assert_equal "Bob", @patron_1.name
     assert_equal 20, @patron_1.spending_money
   end
+
+  def test_starts_with_no_interests
+    assert_equal [], @patron_1.interests
+  end
+
+  def test_it_can_add_interests
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls, Gems and Minerals"], @patron_1.interests
+  end
 end
